c613278b4526d799cb6b216734a935088b203c72
remspoor <remspoor@linuxmail.org>
2019-02-23 17:12:00 +0100

    Change retarction parameters

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1135,9 +1135,9 @@
     #define MIN_AUTORETRACT 0.1           // When auto-retract is on, convert E moves of this length and over
     #define MAX_AUTORETRACT 10.0          // Upper limit for auto-retract conversion
   #endif
-  #define RETRACT_LENGTH 3                // Default retract length (positive mm)
-  #define RETRACT_LENGTH_SWAP 13          // Default swap retract length (positive mm), for extruder change
-  #define RETRACT_FEEDRATE 45             // Default feedrate for retracting (mm/s)
+  #define RETRACT_LENGTH 5                // Default retract length (positive mm)
+  #define RETRACT_LENGTH_SWAP 55          // Default swap retract length (positive mm), for extruder change
+  #define RETRACT_FEEDRATE 70             // Default feedrate for retracting (mm/s)
   #define RETRACT_ZRAISE 0                // Default retract Z-raise (mm)
   #define RETRACT_RECOVER_LENGTH 0        // Default additional recover length (mm, added to retract length when recovering)
   #define RETRACT_RECOVER_LENGTH_SWAP 0   // Default additional swap recover length (mm, added to retract length when recovering from extruder change)
